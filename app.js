63097939338e1d6ec6c364bc2d5e77317818f3d5
Ahmed <ahmedabdulqayyum54@gmail.com>
2024-04-24 23:45:15 +0500

    complet

--- app.js
+++ app.js
@@ -11,16 +11,27 @@
 //     // console.log("eidi", eidi)
 //     eidi.value = total
 //     }
-let eidi = document.getElementById("eidi");
+let eidi = document.getElementById("total-eidi");
+let eidiReceiver = document.getElementById("reciver-input");
 let array =[]
+let myTotalEidi = 0
 
 function distributeEidi(parm){
-    console.log(parm)
+    // console.log(parm)
 
    array.push(parm);
    const total = array.reduce((a,b)=>a+b,0);
    console.log("array",array);
    console.log("total",total);
-   eidi.value = total;
-   
+   eidi.value = total; 
+   myTotalEidi = total
+}
+
+
+function sendEidi(){
+    // let myeidi = document.getElementById("btn")
+    //  alert("suscefully ", myTotalEidi, "eidi send to" , eidiReceiver)
+    // console.log('snedEidi function')
+    alert("Successfully sent "+ myTotalEidi +" to "+eidiReceiver.value)
+
 }
